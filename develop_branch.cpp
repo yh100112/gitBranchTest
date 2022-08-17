986225715b81d9136e7fcace5a20ed3d3f1de22c
yh100112 <yh100112@naver.com>
2022-08-17 14:57:37 +0900

    develop 브랜치에서 파일 수정

--- develop_branch.cpp
+++ develop_branch.cpp
@@ -1,9 +1,12 @@
-adfads
-afasd
-fdsa
-fads
-fasf
-sadf
-asf
-adsf
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+  vector<int16_t> a;
+  a.push_back(3);
+
+  for (auto v : a)
+    cout << v << endl;
+}
